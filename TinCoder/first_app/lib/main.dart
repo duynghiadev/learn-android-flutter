0069511714461f6f8c57bb8550885be9bc1f12e0
duynghiadev <duynghia22302@gmail.com>
2024-12-08 13:32:55 +0700

    [Tự học Flutter - Bài 14] - Cấu trúc dữ liệu List trong ngôn ngữ Dart

--- TinCoder/first_app/lib/main.dart
+++ TinCoder/first_app/lib/main.dart
@@ -1,6 +1,8 @@
 enum Person { tin, hoa, phuong }
 
 var numbers = Iterable.generate(10);
+var list1 = [];
+List<int> numbers2 = [];
 
 void main() {
   int age = 10;
@@ -18,15 +20,50 @@ void main() {
   print(Person.values.length);
   print(Person.values[0]);
 
-  Person.values.forEach((i) {
+  for (var i in Person.values) {
     print('i.name: ${i.name}');
-  });
+  }
+
+  print('----------------------------1------------------------------------');
 
   for (var number in numbers) {
     print('number: ${number}');
   }
 
+  print('-------------------------------2---------------------------------');
+
   print('numbers.length: ${numbers.length}');
   print('numbers.first: ${numbers.first}');
   print('numbers.isNotEmpty: ${numbers.isNotEmpty}');
+
+  // Kiem tra so phan tu
+  print('list1.length: ${list1.length}');
+  print('numbers.length: ${numbers.length}');
+
+  // them phan tu
+  list1.add(1);
+  list1.add(2);
+
+  // Duyet danh sach
+  list1.forEach((i) {
+    print(i.runtimeType);
+    print(i);
+  });
+
+  print('-------------------------------3---------------------------------');
+
+  numbers2.add(1);
+  numbers2.add(2);
+
+  list1.addAll(numbers2);
+  list1.insert(2, 10);
+  list1.forEach((i) {
+    print('i: $i');
+  });
+
+  print('------------------------------4----------------------------------');
+
+  list1.reversed.forEach((i) {
+    print(i);
+  });
 }
